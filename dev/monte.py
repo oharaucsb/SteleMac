4e44a43a353b731628d9f8a8f3b3392fb684c1c2
Cameron Cannon <cameron@andromeda.net>
2020-02-25 03:16:52 -0800

    enabled previous imports and cleaned up code

--- dev/monte.py
+++ dev/monte.py
@@ -1,10 +1,10 @@
-# import hsganalysis.ipg as pg
+import Stele.ipg as pg
 import numpy as np
 import Stele as hsg
 import Stele.QWPProcessing as qwp
 import matplotlib.pyplot as plt
-# from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
-#                               AutoMinorLocator)
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
+                               AutoMinorLocator)
 
 """
 
@@ -156,25 +156,8 @@ for i in range(len(observedSidebands)):
 monteMatrix = np.array(monteMatrix[:, :, 1:])
 
 np.save('monteArray', monteMatrix)
-'''
-# display results for each given sideband
-for i in range(len(observedSidebands)):
 
-    # plot alpha histogram
-    # plt.subplot(AGwidth, 3, 1)
-    # plt.title('Input Error Distribution')
-    for j in range(AGwidth):
-        alphaMu = alphaData[i+1, 2*j+1]
-        alphaSigma = alphaData[i+1, 2*j+2]
-        plt.subplot(AGwidth, 3, (3*j+1))
-        aCount, aBins, aIgnored = plt.hist(monteMatrix[:, 2+j, i],
-                                           30, density=True)
-        plt.plot(aBins, 1/(alphaSigma * np.sqrt(2 * np.pi)) *
-                 np.exp(- (aBins - alphaMu)**2 / (2 * alphaSigma**2)),
-                 linewidth=2, color='r')
-        plt.ylabel('Alpha ' + str(int(excitations[j+1])))
-        plt.yticks([])
-'''
+
 # and save the matrices to text
 # qwp.extractMatrices.saveT(
 # J, observedSidebands, "{}_JMatrix.txt".format(saveFileName))
