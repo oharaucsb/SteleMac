399f51da2fd1252c797589bf0b80214d5dc82034
Cameron Cannon <cameron@andromeda.net>
2020-02-25 18:11:11 -0800

    changed back to 1 iteration and added clarity to subplot properties

--- dev/showMonte.py
+++ dev/showMonte.py
@@ -11,8 +11,8 @@ jones = ['xx', 'xy', 'yx', 'yy']
 observedSidebands = np.array(np.reshape(monteMatrix[1, 1, :], -1))
 
 # display results for each given sideband
-for i in range(len(observedSidebands)):
-    # for i in range(1):
+# for i in range(len(observedSidebands)):
+for i in range(1):
 
     # plot alpha histogram
     # plt.subplot(AGwidth, 3, 1)
@@ -35,41 +35,43 @@ for i in range(len(observedSidebands)):
         alphaMu = monteMatrix[0, 2+(j*2), i]
         alphaSigma = monteMatrix[0, 3+(j*2), i]
         plt.subplot(AGwidth, 3, (3*j+1))
+        plt.ylabel(excitations[j])
+        if j == 0:
+            plt.title('alphas')
+        plt.yticks([])
         aCount, aBins, aIgnored = plt.hist(
             np.reshape(monteMatrix[1:, 2+j, i], -1),
             30, density=True)
         plt.plot(aBins, 1/(alphaSigma * np.sqrt(2 * np.pi)) *
                  np.exp(- (aBins - alphaMu)**2 / (2 * alphaSigma**2)),
                  linewidth=2, color='r')
-        plt.ylabel(excitations[j])
-        if j == 0:
-            plt.title('alphas')
-        plt.yticks([])
 
     # construct gamma histogram subplots
     for j in range(AGwidth):
         gammaMu = monteMatrix[0, 10+(j*2), i]
         gammaSigma = monteMatrix[0, 11+(j*2), i]
         plt.subplot(AGwidth, 3, (3*j+2))
+        plt.ylabel(excitations[j])
+        if j == 0:
+            plt.title('gammas')
+        plt.yticks([])
         aCount, aBins, aIgnored = plt.hist(
             np.reshape(monteMatrix[1:, 6+j, i], -1),
             30, density=True)
         plt.plot(aBins, 1/(gammaSigma * np.sqrt(2 * np.pi)) *
                  np.exp(- (aBins - gammaMu)**2 / (2 * gammaSigma**2)),
                  linewidth=2, color='r')
-        plt.ylabel(excitations[j])
-        if j == 0:
-            plt.title('gammas')
-        plt.yticks([])
 
     # construct jones matrix xy axis scatterplot subplots
     for j in range(4):
         plt.subplot(4, 3, 3*j+3)
-        # do some magic here
         plt.ylabel(jones[j])
         if j == 0:
             plt.title('Jones')
         plt.yticks([])
+        # do some magic here
+        plt.scatter()
+        # end of magic
 
     # fit plot layout and display
     plt.tight_layout()
