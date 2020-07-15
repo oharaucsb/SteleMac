60d223cfc3571e844327cc518ca8ec2da93bdb2b
Cameron <C_Cannon@ucsb.edu>
2020-07-14 23:12:33 -0700

    broke out fullSpectrum, (a work in progress object?) from hsg

---
+++ src/Stele/processing/processing_HSG/FullSpectrum_collection/FullAbsorbance.py
@@ -0,0 +1,8 @@
+class FullAbsorbance(FullSpectrum):
+    """
+    I'm imagining this will sew up absorption spectra, but I'm not at all sure
+    how to do that at the moment.
+    """
+
+    def __init__(self):
+        pass
---
+++ src/Stele/processing/processing_HSG/PMT_collection/TimeTrace.py
@@ -0,0 +1,6 @@
+class TimeTrace(PMT):
+    """
+    This class will be able to handle time traces output by the PMT softare.
+    """
+    def __init__(self, file_path):
+        super(HighSidebandPMT, self).__init__(file_path)
