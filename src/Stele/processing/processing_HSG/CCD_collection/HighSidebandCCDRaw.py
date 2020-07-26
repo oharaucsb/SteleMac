f0d76453dd9b8419543a7854d16da17b3d70064a
Cameron <C_Cannon@ucsb.edu>
2020-07-25 23:00:56 -0700

    conformed CCDRaw to pylama

--- src/Stele/processing/processing_HSG/CCD_collection/HighSidebandCCDRaw.py
+++ src/Stele/processing/processing_HSG/CCD_collection/HighSidebandCCDRaw.py
@@ -1,23 +1,28 @@
 import numpy as np
+import CCD_collection.HighSidebandCCD as HSCCD
 
 np.set_printoptions(linewidth=500)
 
 
-class HighSidebandCCDRaw(HighSidebandCCD):
+class HighSidebandCCDRaw(HSCCD.HighSidebandCCD):
     """
-    This class is meant for passing in an image file (currently supports a 2x1600)
+    This class is meant for passing in an image file
+        (currently supports a 2x1600)
     Which it does all the processing on.
     """
-    def __init__(self, hsg_thing, parameter_dict=None, spectrometer_offset=None):
-        # let the supers do the hard work of importing the json dict and all that jazz
-        super(HighSidebandCCDRaw, self).__init__(hsg_thing, parameter_dict=None, spectrometer_offset=None)
+    def __init__(
+     self, hsg_thing, parameter_dict=None, spectrometer_offset=None):
+        # let the supers do the hard work of importing the
+        # json dict and all that jazz
+        super(HighSidebandCCDRaw, self).__init__(
+            hsg_thing, parameter_dict=None, spectrometer_offset=None)
         self.ccd_data = np.genfromtxt(hsg_thing, delimiter=',').T
-        self.proc_data = np.column_stack((
-            self.gen_wavelengths(self.parameters["center_lambda"], self.parameters["grating"]),
-            np.array(self.ccd_data[:,1], dtype=float)-np.median(self.ccd_data[:,1]),
-            np.ones_like(self.ccd_data[:,1], dtype=float)
-                                         ))
-
+        self.proc_data = np.column_stack((self.gen_wavelengths(
+            self.parameters["center_lambda"],
+            self.parameters["grating"]), np.array(self.ccd_data[:, 1],
+                                                  dtype=float)
+            - np.median(self.ccd_data[:, 1]),
+            np.ones_like(self.ccd_data[:, 1], dtype=float)))
 
         self.proc_data[:, 0] = 1239.84 / self.proc_data[:, 0]
         self.proc_data = np.flipud(self.proc_data)
@@ -25,7 +30,8 @@ class HighSidebandCCDRaw(HighSidebandCCD):
     @staticmethod
     def gen_wavelengths(center_lambda, grating):
         '''
-        This returns a 1600 element list of wavelengths for each pixel in the EMCCD based on grating and center wavelength
+        This returns a 1600 element list of wavelengths for each pixel in the
+            EMCCD based on grating and center wavelength
 
         grating = which grating, 1 or 2
         center = center wavelength in nanometers
@@ -53,16 +59,23 @@ class HighSidebandCCDRaw(HighSidebandCCD):
         center = center_lambda * 10 ** -9
         wavelength_list = np.arange(-799.0, 801.0)
 
-        output = d * k ** (-1) * ((-1) * np.cos(delta + gamma + (-1) * np.arccos(
-            (-1 / 4) * (1 / np.cos((1 / 2) * gamma)) ** 2 * (
-            2 * (np.cos((1 / 2) * gamma) ** 4 * (2 + (-1) * d ** (-2) * k ** 2 * center ** 2 + 2 * np.cos(gamma))) ** (
-            1 / 2) + d ** (-1) * k * center * np.sin(gamma))) + np.arctan(
-            b ** (-1) * (r * wavelength_list + b * np.cos(delta + gamma)) * (1 / np.sin(delta + gamma)))) + (
-                                  1 + (-1 / 16) * (1 / np.cos((1 / 2) * gamma)) ** 4 * (2 * (
-                                  np.cos((1 / 2) * gamma) ** 4 * (
-                                  2 + (-1) * d ** (-2) * k ** 2 * center ** 2 + 2 * np.cos(gamma))) ** (1 / 2) + d ** (
-                                                                                        -1) * k * center * np.sin(
-                                      gamma)) ** 2) ** (1 / 2))
+        gammaNorm = np.cos((1 / 2) * gamma)
+
+        # TODO: make this output from the elder gods self documenting
+        output = d * k ** (-1) * ((-1) * np.cos(
+            delta + gamma + (-1) * np.arccos(
+                (-1 / 4) * (1 / np.cos((1 / 2) * gamma)) ** 2 *
+                (2 * (np.cos((1 / 2) * gamma) ** 4 * (2 + (-1) * d ** (-2) * k
+                      ** 2 * center ** 2 + 2 * np.cos(gamma)))
+                 ** (1 / 2) + d ** (-1) * k * center * np.sin(gamma)))
+            + np.arctan(b ** (-1) * (r * wavelength_list + b *
+                        np.cos(delta + gamma)) * (1 / np.sin(delta + gamma))))
+                        + (1 + (-1 / 16) * (1 / np.cos((1 / 2) * gamma)) ** 4 *
+                           (2 * (np.cos((1 / 2) * gamma) ** 4 * (2 + (-1) * d
+                            ** (-2) * k ** 2 * center ** 2 + 2 *
+                            np.cos(gamma)))
+                            ** (1 / 2) + d ** (-1) * k * center * np.sin(gamma)
+                            ) ** 2) ** (1 / 2))
 
         output = (output + center) * 10 ** 9
         return output
