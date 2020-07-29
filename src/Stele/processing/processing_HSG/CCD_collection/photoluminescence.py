c0ee44db1a2529d28247310ec7b667396f82603d
Cameron <C_Cannon@ucsb.edu>
2020-07-29 15:27:44 -0700

    conformed photoluminescence to pylama

--- src/Stele/processing/processing_HSG/CCD_collection/photoluminescence.py
+++ src/Stele/processing/processing_HSG/CCD_collection/photoluminescence.py
@@ -1,11 +1,13 @@
 import numpy as np
+from .CCD_collection import CCD
 
-class Photoluminescence(CCD):
+
+class Photoluminescence(CCD.CCD):
     def __init__(self, fname):
         """
-        This object handles PL-type data. The only distinction from the parent class
-        is that the CCD data gets normalized to the exposure time to make different
-        exposures directly comparable.
+        This object handles PL-type data. The only distinction from the parent
+        class is that the CCD data gets normalized to the exposure time to make
+        different exposures directly comparable.
 
         creates:
         self.proc_data = self.ccd_data divided by the exposure time
@@ -16,8 +18,10 @@ class Photoluminescence(CCD):
         """
         super(Photoluminescence, self).__init__(fname)
 
-        # Create a copy of the array , and then normalize the signal and the errors
-        # by the exposure time
+        # Create a copy of the array , and then normalize the signal and the
+        #  errors by the exposure time
         self.proc_data = np.array(self.ccd_data)
-        self.proc_data[:, 1] = self.proc_data[:, 1] / self.parameters['exposure']
-        self.proc_data[:, 2] = self.proc_data[:, 2] / self.parameters['exposure']
+        self.proc_data[:, 1] = (self.proc_data[:, 1]
+                                / self.parameters['exposure'])
+        self.proc_data[:, 2] = (self.proc_data[:, 2]
+                                / self.parameters['exposure'])
