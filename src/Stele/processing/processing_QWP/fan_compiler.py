c02d14334ef81616b3594321d9415c7998ce575e
Cameron <C_Cannon@ucsb.edu>
2020-08-20 16:12:49 -0700

    initial fan_compiler pylama compliance

--- src/Stele/processing/processing_QWP/fan_compiler.py
+++ src/Stele/processing/processing_QWP/fan_compiler.py
@@ -68,8 +68,8 @@ class FanCompiler(object):
             self._n = -1
 
     @staticmethod
-    def fromDataFolder(folder, wantedSBs, keepErrors = False, negateNIR = True,
-                       eta=None, doNorms = False):
+    def fromDataFolder(folder, wantedSBs, keepErrors=False, negateNIR=True,
+                       eta=None, doNorms=False):
         """
         Create a fan compiler by passing the data path. Handles looping through
             the folder's sub-folders to find
@@ -88,15 +88,14 @@ class FanCompiler(object):
 
         for nirFolder in wantFolders:
             # Provide ways to skip over data sets by mis-naming/flagging them
-            if "skip" in nirFolder.lower() or "bad" in nirFolder.lower(): continue
-            laserParams, rawData = hsg.hsg_combine_qwp_sweep(nirFolder, save=False,
-                                                             verbose=False,
-                                                             loadNorm=doNorms)
+            if "skip" in nirFolder.lower() or "bad" in nirFolder.lower():
+                continue
+            laserParams, rawData = hsg.hsg_combine_qwp_sweep(
+                nirFolder, save=False, verbose=False, loadNorm=doNorms)
 
-            _, fitDict = hsg.proc_n_fit_qwp_data(rawData, laserParams,
-                                                 vertAnaDir="VAna" in nirFolder,
-                                                 series=nirFolder,
-                                                 eta=eta)
+            _, fitDict = hsg.proc_n_fit_qwp_data(
+                rawData, laserParams, vertAnaDir="VAna" in nirFolder,
+                series=nirFolder, eta=eta)
             comp.addSet(fitDict)
         return comp
 
@@ -104,15 +103,14 @@ class FanCompiler(object):
         """ Assume it's passed from  proc_n_fit_qwp_data, assuming it is a dict
         with the keys and shape returned by that function"""
 
-        # Keeps track of all the new data passed. Keys are the angles/S parameters,
-        # values are arrays for each of those which will contain the value of the
-        # parameters for a given sideband order
+        # Keeps track of all the new data passed. Keys are the angles/S
+        # parameters, values are arrays for each of those which will contain
+        # the value of the parameters for a given sideband order
         newData = {ii: [] for ii in self.outputArrays}
 
         # new data needs to parsed to extract the relevant parameters for all
-        # the sidebands specified in this FanCompiler's constructor. Data is kept as
-        # a list of lists in `newData`
-
+        # the sidebands specified in this FanCompiler's constructor. Data is
+        # kept as a list of lists in `newData`
 
         if self._e:
             # keep track of the errors in everything
@@ -122,8 +120,10 @@ class FanCompiler(object):
                 # the list(*[]) bullshit is to make sure a list gets appended,
                 # not a numpy array. Further complicated because if the list
                 # comprehension returns nothing, it doesn't append anything,
-                # hence casting to a list to enforce an empty list gets appended.
-                [jj.append( list(*[ii[1:] for ii in dataSet[kk] if ii[0] == sb]) )
+                # hence casting to a list to enforce an empty list gets
+                # appended.
+                [jj.append(list(*[ii[1:] for ii in dataSet[kk] if
+                           ii[0] == sb]))
                  for kk, jj in newData.items()]
                 if not newData["alpha"][-1]:
                     # no data was found, so the last element is an empty array.
@@ -132,51 +132,56 @@ class FanCompiler(object):
                     for jj in newData.values():
                         jj[-1] = [np.nan, np.nan]
         else:
-            # Even though it's a single number each, put them in lists so they can
-            # be list.extended() and consistent with the error usage above
+            # Even though it's a single number each, put them in lists so they
+            # can be list.extended() and consistent with the error usage above
             nirAlpha = [self._n*dataSet["alpha"][0][1]]
             nirGamma = [dataSet["gamma"][0][1]]
             for sb in self.want:
-                # Even though only one element is being kept (i.e. data without error),
-                # it's still being placed inside a list to be consistent with the
-                # use case with errors above
-                [jj.append( list([ii[1] for ii in dataSet[kk] if ii[0] == sb]) )
+                # Even though only one element is being kept (i.e. data without
+                # error), it's still being placed inside a list to be
+                # consistent with the use case with errors above
+                [jj.append(list([ii[1] for ii in dataSet[kk] if ii[0] == sb]))
                  for kk, jj in newData.items()]
-                if not newData["alpha"][-1]: # no data was found.
+                # no data was found.
+                if not newData["alpha"][-1]:
                     for jj in newData.values():
                         jj[-1] = [np.nan]
 
         for k, v in newData.items():
             self.outputArrays[k] = np.column_stack((self.outputArrays[k], v))
 
-
         # extending created lists accounts for keeping errors r not
         self.nirAlphas.extend(nirAlpha)
         self.nirGammas.extend(nirGamma)
 
-    def build(self, withErrors = True):
+    def build(self, withErrors=True):
         """
-        Return only alpha, gamma, S0 parameters directly, for compatibility and ease.
+        Return only alpha, gamma, S0 parameters directly, for compatibility and
+            ease.
         :param withErrors:
         :return:
         """
         data = self.buildAll()
         if not self._e:
-            # You didn't keep track of errors when loading datasets, so just return
-            # the data sets
+            # You didn't keep track of errors when loading datasets, so just
+            # return the data sets
             return data["alpha"], data["gamma"], data["S0"]
 
         if withErrors:
             return data["alpha"], data["gamma"], data["S0"]
-        alphaData = np.column_stack((data["alpha"][:, 0], data["alpha"][:, 1::2]))
-        gammaData = np.column_stack((data["gamma"][:, 0], data["gamma"][:, 1::2]))
+        alphaData = np.column_stack((
+            data["alpha"][:, 0], data["alpha"][:, 1::2]
+            ))
+        gammaData = np.column_stack((
+            data["gamma"][:, 0], data["gamma"][:, 1::2]
+            ))
         S0Data = np.column_stack((data["S0"][:, 0], data["S0"][:, 1::2]))
         return alphaData, gammaData, S0Data
 
-
     def buildAll(self):
         """
-        a dict of d[<S0,S1,S2,S3,alpha, gamma, DOP>] where each item is a 2D array:
+        a dict of d[<S0,S1,S2,S3,alpha, gamma, DOP>] where each item is a
+            2D array:
 
                  -1  |   NIRa   |   dNIR err  |  NIRa2   |  dNIRa err  |    ...
               sb1    | SB Data  | SB Data err | SB Data  | SB Data err |    ...
@@ -188,9 +193,10 @@ class FanCompiler(object):
 
         Errors are not included if not passed in the fanCompiler constructor
 
-        return["gamma"] replaces the first row with the NIR gamma values, which was
-        useful when doing polarimetry with non-linearly polarized light (i.e. circular)
-        Furthermore, these values get passed to the Jones matrix extraction stuff
+        return["gamma"] replaces the first row with the NIR gamma values, which
+        was useful when doing polarimetry with non-linearly polarized light
+        (i.e. circular) Furthermore, these values get passed to the Jones
+        matrix extraction stuff
 
         :return:
         """
@@ -206,16 +212,18 @@ class FanCompiler(object):
 
     def buildAndSave(self, fname, *args, saveStokes=False):
         """
-        fname: filename to save to. Must have a least one string formatter position
-        to allow for saving separate alpha/gamma/s0 files. *args are passed to any
-        other formatting positions.
+        fname: filename to save to. Must have a least one string formatter
+            position to allow for saving separate alpha/gamma/s0 files. *args
+            are passed to any other formatting positions.
         :param fname:
         :param args:
         :param saveStokes: Pass true if you want to save the stokes files
         :return:
         """
 
-        if os.path.dirname(fname) and not os.path.exists(os.path.dirname(fname)):
+        if os.path.dirname(fname) and not os.path.exists(
+                os.path.dirname(fname)):
+
             os.mkdir(os.path.dirname(fname))
 
         oh = "#\n" * 100
@@ -232,20 +240,4 @@ class FanCompiler(object):
 
         for saveEm in saveEms:
             np.savetxt(fname.format(saveEm, *args), outputs[saveEm], header=oh,
-                       delimiter=',',
-                       comments='')
-
-        # # fullData = np.append([-1], self.nirAlphas)
-        # # fullData = np.row_stack((fullData, self.arrA))
-        # np.savetxt(fname.format("alpha", *args), fullDataA, header=oh, delimiter=',',
-        #            comments='')
-        #
-        # # fullData = np.append([-1], self.nirAlphas)
-        # # fullData = np.row_stack((fullData, self.arrG))
-        # np.savetxt(fname.format("gamma", *args), fullDataG, header=oh, delimiter=',',
-        #            comments='')
-        #
-        # # fullData = np.append([-1], self.nirAlphas)
-        # # fullData = np.row_stack((fullData, self.arrS))
-        # np.savetxt(fname.format("S0", *args), fullDataS, header=oh, delimiter=',',
-        #            comments='')
+                       delimiter=',', comments='')
