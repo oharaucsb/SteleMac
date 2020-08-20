bd52d77699e357c30ee37b06af0c913e4be4b50e
Cameron <C_Cannon@ucsb.edu>
2020-08-20 15:01:54 -0700

    removed block of out commented code

--- src/Stele/processing/processing_QWP/polarPlot.py
+++ src/Stele/processing/processing_QWP/polarPlot.py
@@ -31,8 +31,6 @@ def polarPlot(*args, **kwargs):
 
     crv.setData = newSetData
 
-    # crv.setData(r, theta)
-
     return crv
 def polarPlotBad(*args, **kwargs):
 
@@ -51,27 +49,3 @@ def polarPlotBad(*args, **kwargs):
     crv.setData(*args, **kwargs)
 
     return crv
-
-
-
-#
-# plot = pg.plot()
-# plot.setAspectLocked()
-#
-# # Add polar grid lines
-# plot.addLine(x=0, pen=0.2)
-# plot.addLine(y=0, pen=0.2)
-# for r in range(2, 20, 2):
-#     circle = pg.QtGui.QGraphicsEllipseItem(-r, -r, r*2, r*2)
-#     circle.setPen(pg.mkPen(0.2))
-#     plot.addItem(circle)
-#
-# # make polar data
-# import numpy as np
-# theta = np.linspace(0, 2*np.pi, 100)
-# radius = np.random.normal(loc=10, size=100)
-#
-# # Transform to cartesian and plot
-# x = radius * np.cos(theta)
-# y = radius * np.sin(theta)
-# plot.plot(x, y)
