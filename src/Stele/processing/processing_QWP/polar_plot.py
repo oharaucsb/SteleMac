3d68d2a9f477bcd0a5c0886bb4ae0d22e98589e5
Cameron <C_Cannon@ucsb.edu>
2020-08-20 19:00:59 -0700

    complied polar_plot to pylama

--- src/Stele/processing/processing_QWP/polar_plot.py
+++ src/Stele/processing/processing_QWP/polar_plot.py
@@ -1,7 +1,7 @@
 from __future__ import division
 import numpy as np
 import Stele.ipg as pg
-# import pyqtgraph as pg
+
 
 def makePolarPlot():
     plot = pg.figure()
@@ -11,7 +11,7 @@ def makePolarPlot():
     plot.addLine(y=0, pen=0.2)
     for r in range(4, 38, 2):
         circle = pg.QtGui.QGraphicsEllipseItem(-r, -r, r * 2, r * 2)
-        circle.setPen(pg.pg.mkPen(0.2, width=1+1*(r==18)))
+        circle.setPen(pg.pg.mkPen(0.2, width=1+1*(r == 18)))
         plot.addItem(circle)
     return plot
 
@@ -23,29 +23,27 @@ def polarPlot(*args, **kwargs):
     crv = plt.plot(*args, **kwargs)
 
     old = crv.setData
-    def newSetData(r, theta, *args, **kwargs):
 
+    def newSetData(r, theta, *args, **kwargs):
         x = r * np.cos(theta)
         y = r * np.sin(theta)
         return old(x, y, *args, **kwargs)
 
     crv.setData = newSetData
-
     return crv
+
+
 def polarPlotBad(*args, **kwargs):
 
     plt = pg.gcf()
-
     crv = plt.plot()
-
     old = crv.setData
+
     def newSetData(self, *args, **kwargs):
         old(self, *args, **kwargs)
         x = self.xData * np.cos(self.yData)
         y = self.xData * np.sin(self.yData)
 
     crv.setData = newSetData
-
     crv.setData(*args, **kwargs)
-
     return crv
