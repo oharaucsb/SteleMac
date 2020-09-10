c5a886bf377bc55519fe4e772d2745c0fe9ab695
Cameron <C_Cannon@ucsb.edu>
2020-09-09 17:45:45 -0700

    conformed collection_functions.py to pylama

--- src/Stele/analysis/analysis_hsg/collection_functions.py
+++ src/Stele/analysis/analysis_hsg/collection_functions.py
@@ -384,28 +384,34 @@ def hsg_combine_qwp_sweep(
                 # from back before I changed the name
                 keyName = "detectorHWP"
 
-            save_parameter_sweep(specs, [full, snip, norm], None,
-                                     keyName, "deg", wanted_indices=[3, 4],
-                                     header_dict={
-                                         "lAlpha": specs[0].parameters["nir_pola"],
-                                         "lGamma": specs[0].parameters["nir_polg"],
-                                         "nir": specs[0].parameters["nir_lambda"],
-                                         "thz": specs[0].parameters["fel_lambda"], },
-                                 only_even=skipOdds)
+            save_parameter_sweep(
+                specs, [full, snip, norm], None, keyName, "deg",
+                wanted_indices=[3, 4],
+                header_dict={
+                    "lAlpha": specs[0].parameters["nir_pola"],
+                    "lGamma": specs[0].parameters["nir_polg"],
+                    "nir": specs[0].parameters["nir_lambda"],
+                    "thz": specs[0].parameters["fel_lambda"],
+                    },
+                only_even=skipOdds
+                )
 
             if loadNorm:
                 sbData, lAlpha, lGamma, nir, thz = getData(norm)
             else:
                 sbData, lAlpha, lGamma, nir, thz = getData(snip)
         else:
-            save_parameter_sweep(specs, os.path.basename(path), "Processed QWP Dependence",
-                                 "rotatorAngle", "deg", wanted_indices=[3, 4],
-                                 header_dict={
-                                     "lAlpha": specs[0].parameters["nir_pola"],
-                                     "lGamma": specs[0].parameters["nir_polg"],
-                                     "nir": specs[0].parameters["nir_lambda"],
-                                     "thz": specs[0].parameters["fel_lambda"], },
-                                 only_even=skipOdds)
+            save_parameter_sweep(
+                specs, os.path.basename(path), "Processed QWP Dependence",
+                "rotatorAngle", "deg", wanted_indices=[3, 4],
+                header_dict={
+                    "lAlpha": specs[0].parameters["nir_pola"],
+                    "lGamma": specs[0].parameters["nir_polg"],
+                    "nir": specs[0].parameters["nir_lambda"],
+                    "thz": specs[0].parameters["fel_lambda"],
+                    },
+                only_even=skipOdds
+                )
             sbData, lAlpha, lGamma, nir, thz = getData(os.path.basename(path))
 
     laserParams = {
@@ -419,16 +425,19 @@ def hsg_combine_qwp_sweep(
     # first two rows are origin header, second is sideband number
     # (and empty strings, which is why the "if ii" below, to prevent
     # ValueErrors on int('').
-    foundSidebands = np.array(sorted([float(ii) for ii in set(sbData[2]) if ii]))
+    foundSidebands = np.array(
+        sorted([float(ii) for ii in set(sbData[2]) if ii]))
 
-    # Remove first 3 rows, which are strings for origin header, and cast it to floats
+    # Remove first 3 rows, which are strings for origin header, & cast to float
     sbData = sbData[3:].astype(float)
 
     # double the sb numbers (to account for sb strength/error) and add a dummy
     # number so the array is the same shape
-    foundSidebands = np.insert(foundSidebands, range(len(foundSidebands)), foundSidebands)
+    foundSidebands = np.insert(
+        foundSidebands, range(len(foundSidebands)), foundSidebands)
     foundSidebands = np.insert(foundSidebands, 0, -1)
     return laserParams, np.row_stack((foundSidebands, sbData))
+
 
 def makeCurve(eta, isVertical):
     """
@@ -444,37 +453,46 @@ def makeCurve(eta, isVertical):
         def analyzerCurve(x, *S):
             S0, S1, S2, S3 = S
             return S0-S1/2*(1+np.cos(eta)) \
-                   + S3*np.sin(eta)*sind(2*x) \
-                   + S1/2*(np.cos(eta)-1)*cosd(4*x) \
-                   + S2/2*(np.cos(eta)-1)*sind(4*x)
+                + S3*np.sin(eta)*sind(2*x) \
+                + S1/2*(np.cos(eta)-1)*cosd(4*x) \
+                + S2/2*(np.cos(eta)-1)*sind(4*x)
     else:
         # vertical polarizer
         def analyzerCurve(x, *S):
             S0, S1, S2, S3 = S
             return S0+S1/2*(1+np.cos(eta)) \
-                   - S3*np.sin(eta)*sind(2*x) \
-                   + S1/2*(1-np.cos(eta))*cosd(4*x) \
-                   + S2/2*(1-np.cos(eta))*sind(4*x)
+                - S3*np.sin(eta)*sind(2*x) \
+                + S1/2*(1-np.cos(eta))*cosd(4*x) \
+                + S2/2*(1-np.cos(eta))*sind(4*x)
     return analyzerCurve
 
-def proc_n_fit_qwp_data(data, laserParams = dict(), wantedSBs = None, vertAnaDir = True, plot=False,
-                        save = False, plotRaw = lambda sbidx, sbnum: False, series = '', eta=None,
-                        **kwargs):
+
+def proc_n_fit_qwp_data(
+        data, laserParams=dict(), wantedSBs=None, vertAnaDir=True, plot=False,
+        save=False, plotRaw=lambda sbidx, sbnum: False, series='', eta=None,
+        **kwargs
+        ):
     """
     Fit a set of sideband data vs QWP angle to get the stoke's parameters
     :param data: data in the form of the return of hsg_combine_qwp_sweep
-    :param laserParams: dictionary of the parameters of the laser, the angles and frequencies. See function for
-                expected keys. I don't think the errors are used (except for plotting?), or the wavelengths (but
-                left in for potential future use (wavelength dependent stuff?))
+    :param laserParams: dictionary of the parameters of the laser, the angles
+        and frequencies. See function for expected keys. I don't think the
+        errors are used (except for plotting?), or the wavelengths (but left in
+        for potential future use (wavelength dependent stuff?))
     :param wantedSBs: List of the wanted sidebands to fit out.
-    :param vertAnaDir: direction of the analzyer. True if vertical, false if horizontal.
-    :param plot: True/False to plot alpha/gamma/dop. Alternatively, a list of "a", "g", "d" to only plot selected ones
+    :param vertAnaDir: direction of the analzyer. True if vertical, false if
+        horizontal.
+    :param plot: True/False to plot alpha/gamma/dop. Alternatively, a list of
+        "a", "g", "d" to only plot selected ones
     :param save: filename to save the files. Accepts BytesIO
-    :param plotRaw: callable that takes an index of the sb and sb number, returns true to plot the raw curve
+    :param plotRaw: callable that takes an index of the sb and sb number,
+        returns true to plot the raw curve
     :param series: a string to be put in the header for the origin files
-    :param eta: a function to call to calculate the desired retardance. Input will be the SB order.
+    :param eta: a function to call to calculate the desired retardance. Input
+        will be the SB order.
 
-    if saveStokes is in kwargs and False, it will not save the stokes parameters, since I rarely actually use them.
+    if saveStokes is in kwargs and False, it will not save the stokes
+        parameters, since I rarely actually use them.
     :return:
     """
     defaultLaserParams = {
@@ -488,25 +506,17 @@ def proc_n_fit_qwp_data(data, laserParams = dict(), wantedSBs = None, vertAnaDir
         "thz": 21.1
     }
     defaultLaserParams.update(laserParams)
-    lAlpha, ldAlpha, lGamma, ldGamma, lDOP, ldDOP = defaultLaserParams["lAlpha"], \
-                                                    defaultLaserParams["ldAlpha"], \
-                                                    defaultLaserParams["lGamma"], \
-                                                    defaultLaserParams["ldGamma"], \
-                                                    defaultLaserParams["lDOP"], \
-                                                    defaultLaserParams["ldDOP"]
+    lAlpha, ldAlpha, lGamma, ldGamma, lDOP, ldDOP = \
+        defaultLaserParams["lAlpha"], \
+        defaultLaserParams["ldAlpha"], \
+        defaultLaserParams["lGamma"], \
+        defaultLaserParams["ldGamma"], \
+        defaultLaserParams["lDOP"], \
+        defaultLaserParams["ldDOP"]
     allSbData = data
     angles = allSbData[1:, 0]
 
-    # angles += -5
-    # print("="*20)
-    # print("\n"*3)
-    # print("             WARNING")
-    # print("\n"*3)
-    # print("ANGLES HAVE BEEN MANUALLY OFFEST IN proc_n_fit_qwp_data")
-    # print("\n"*3)
-    # print("="*20)
-
-    allSbData = allSbData[:, 1:] # trim out the angles
+    allSbData = allSbData[:, 1:]  # trim out the angles
 
     if wantedSBs is None:
         # set to get rid of duplicates, 1: to get rid of the -1 used for
@@ -515,13 +525,15 @@ def proc_n_fit_qwp_data(data, laserParams = dict(), wantedSBs = None, vertAnaDir
 
     if eta is None:
         """
-        It might be easier for the end user to do this by passing eta(wavelength) instead of eta(sborder),
-        but then this function would need to carry around wavelengths, which is extra work. It could convert
-        between NIR/THz wavelengths to SB order, but it's currently unclear whether you'd rather use what the WS6
-        claims, or what the sidebands say, and you'd probably want to take the extra step to ensure the SB fit rseults
-        if using the spectromter wavelengths. In general, if you have a function as etal(wavelength), you'd probably
-        want to pass this as
-        eta = lambda x: etal(1239.84/(nirEv + x*THzEv))
+        It might be easier for the end user to do this by passing
+        eta(wavelength) instead of eta(sborder), but then this function would
+        need to carry around wavelengths, which is extra work. It could convert
+        between NIR/THz wavelengths to SB order, but it's currently unclear
+        whether you'd rather use what the WS6 claims, or what the sidebands
+        say, and you'd probably want to take the extra step to ensure the SB
+        fit rseults if using the spectromter wavelengths. In general, if you
+        have a function as etal(wavelength), you'd probably want to pass this
+        as eta = lambda x: etal(1239.84/(nirEv + x*THzEv))
         assuming nirEv/THzEv are the photon energies of the NIR/THz.
         """
         eta = lambda x: 0.25
@@ -531,10 +543,12 @@ def proc_n_fit_qwp_data(data, laserParams = dict(), wantedSBs = None, vertAnaDir
     skipOdds = kwargs.get("skip_odds", True)
 
     # Make an array to keep all of the sideband information.
-    # Start it off by keeping the NIR information (makes for easier plotting into origin)
+    # Start it off by keeping the NIR information
+    #   (makes for easier plotting into origin)
     sbFits = [[0] + [-1] * 8 + [lAlpha, ldAlpha, lGamma, ldGamma, lDOP, ldDOP]]
     # Also, for convenience, keep a dictionary of the information.
-    # This is when I feel like someone should look at porting this over to pandas
+    # This is when I feel like someone should look at porting this
+    # over to pandas
     sbFitsDict = {}
     sbFitsDict["S0"] = [[0, -1, -1]]
     sbFitsDict["S1"] = [[0, -1, -1]]
@@ -547,48 +561,39 @@ def proc_n_fit_qwp_data(data, laserParams = dict(), wantedSBs = None, vertAnaDir
     # Iterate over all sb data. Skip by 2 because error bars are included
     for sbIdx in range(0, allSbData.shape[1], 2):
         sbNum = allSbData[0, sbIdx]
-        if sbNum not in wantedSBs: continue
-        if skipOdds and sbNum%2: continue
-        # if verbose:
-        #     print("\tlooking at sideband", sbNum)
+        if sbNum not in wantedSBs:
+            continue
+        if skipOdds and sbNum%2:
+            continue
         sbData = allSbData[1:, sbIdx]
         sbDataErr = allSbData[1:, sbIdx + 1]
-
-        # try:
-        #     p0 = sbFits[-1][1:8:2]
-        # except:
-        #     p0 = [1, 1, 0, 0]
         p0 = [1, 1, 0, 0]
 
         etan = eta(sbNum)
         try:
-            p, pcov = curve_fit(makeCurve(etan, vertAnaDir), angles, sbData, p0=p0)
+            p, pcov = curve_fit(
+                makeCurve(etan, vertAnaDir), angles, sbData, p0=p0
+                )
         except ValueError:
-            # This is getting tossed around, especially when looking at noisy data,
-            # especially with the laser line, and it's fitting erroneous values.
-            # Ideally, I should be cutting this out and not even returning them,
-            # but that's immedaitely causing
+            # This is getting tossed around, especially when looking at noisy
+            # data, especially with the laser line, and it's fitting erroneous
+            # values.
+            # Ideally, I should be cutting this out and not even returning
+            # them, but that's immedaitely causing
             p = np.nan*np.array(p0)
             pcov = np.eye(len(p))
 
-
         if plot and plotRaw(sbIdx, sbNum):
-            # pg.figure("{}: sb {}".format(dataName, sbNum))
             plt.figure("All Curves")
-            plt.errorbar(angles, sbData, sbDataErr, 'o-', name=f"{series}, {sbNum}")
-            # plt.plot(angles, sbData,'o-', label="Data")
+            plt.errorbar(
+                angles, sbData, sbDataErr, 'o-', name=f"{series}, {sbNum}")
             fineAngles = np.linspace(angles.min(), angles.max(), 300)
-            # plt.plot(fineAngles,
-            #         makeCurve(eta, "V" in dataName)(fineAngles, *p0), name="p0")
-            plt.plot(fineAngles,
-                    makeCurve(etan, vertAnaDir)(fineAngles, *p))
-            # plt.show()
+            plt.plot(fineAngles, makeCurve(etan, vertAnaDir)(fineAngles, *p))
             plt.ylim(0, 1)
             plt.xlim(0, 360)
             plt.ylabel("Normalized Intensity")
             plt.xlabel("QWP Angle (&theta;)")
             print(f"\t{series} {sbNum}, p={p}")
-
 
         # get the errors
         d = np.sqrt(np.diag(pcov))
@@ -606,23 +611,35 @@ def proc_n_fit_qwp_data(data, laserParams = dict(), wantedSBs = None, vertAnaDir
         # append alpha value
         thisData.append(np.arctan2(S2, S1) / 2 * 180. / np.pi)
         # append alpha error
-        variance = (d2 ** 2 * S1 ** 2 + d1 ** 2 * S2 ** 2) / (S1 ** 2 + S2 ** 2) ** 2
+        variance = \
+            (d2 ** 2 * S1 ** 2 + d1 ** 2 * S2 ** 2) \
+            / (S1 ** 2 + S2 ** 2) ** 2
         thisData.append(np.sqrt(variance) * 180. / np.pi)
 
         sbFitsDict["alpha"].append([sbNum, thisData[-2], thisData[-1]])
 
         # append gamma value
-        thisData.append(np.arctan2(S3, np.sqrt(S1 ** 2 + S2 ** 2)) / 2 * 180. / np.pi)
+        thisData.append(
+            np.arctan2(S3, np.sqrt(S1 ** 2 + S2 ** 2)) / 2 * 180. / np.pi)
         # append gamma error
-        variance = (d3 ** 2 * (S1 ** 2 + S2 ** 2) ** 2 + (d1 ** 2 * S1 ** 2 + d2 ** 2 * S2 ** 2) * S3 ** 2) / (
-        (S1 ** 2 + S2 ** 2) * (S1 ** 2 + S2 ** 2 + S3 ** 2) ** 2)
+        variance = (
+            d3 ** 2 * (S1 ** 2 + S2 ** 2) ** 2
+            + (d1 ** 2 * S1 ** 2 + d2 ** 2 * S2 ** 2) * S3 ** 2
+            ) \
+            / ((S1 ** 2 + S2 ** 2) * (S1 ** 2 + S2 ** 2 + S3 ** 2) ** 2)
         thisData.append(np.sqrt(variance) * 180. / np.pi)
         sbFitsDict["gamma"].append([sbNum, thisData[-2], thisData[-1]])
 
         # append degree of polarization
         thisData.append(np.sqrt(S1 ** 2 + S2 ** 2 + S3 ** 2) / S0)
-        variance = ((d1 ** 2 * S0 ** 2 * S1 ** 2 + d0 ** 2 * (S1 ** 2 + S2 ** 2 + S3 ** 2) ** 2 + S0 ** 2 * (
-        d2 ** 2 * S2 ** 2 + d3 ** 2 * S3 ** 2)) / (S0 ** 4 * (S1 ** 2 + S2 ** 2 + S3 ** 2)))
+        variance = (
+            (
+                d1 ** 2 * S0 ** 2 * S1 ** 2
+                + d0 ** 2 * (S1 ** 2 + S2 ** 2 + S3 ** 2) ** 2
+                + S0 ** 2 * (d2 ** 2 * S2 ** 2 + d3 ** 2 * S3 ** 2)
+            )
+            / (S0 ** 4 * (S1 ** 2 + S2 ** 2 + S3 ** 2))
+        )
         thisData.append(np.sqrt(variance))
         sbFitsDict["DOP"].append([sbNum, thisData[-2], thisData[-1]])
 
@@ -630,18 +647,13 @@ def proc_n_fit_qwp_data(data, laserParams = dict(), wantedSBs = None, vertAnaDir
 
     sbFits = np.array(sbFits)
     sbFitsDict = {k: np.array(v) for k, v in sbFitsDict.items()}
-    # This chunk used to insert the "alpha deviation", the difference between the angles and the
-    # nir. I don't think I use this anymore, so stop saving it
-                # origin_header = 'Sideband,S0,S0 err,S1,S1 err,S2,S2 err,S3,S3 err,alpha,alpha deviation,alpha err,gamma,gamma err,DOP,DOP err\n'
-                # origin_header += 'Order,arb.u,arb.u,arb.u,arb.u,arb.u,arb.u,arb.u,arb.u,deg,deg,deg,deg,deg,arb.u.,arb.u.\n'
-                # origin_header += 'Sideband,{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}'.format(*["{}".format(series)] * 15)
-                # sbFits = np.array(sbFits)
-                # sbFits = np.insert(sbFits, 10, sbFits[:, 9] - lAlpha, axis=1)
-                # sbFits = sbFits[sbFits[:, 0].argsort()]
 
-    origin_header = "#\n"*100 # to fit all other files for easy origin importing
-    origin_header += 'Sideband,S0,S0 err,S1,S1 err,S2,S2 err,S3,S3 err,alpha,alpha err,gamma,gamma err,DOP,DOP err\n'
-    origin_header += 'Order,arb.u,arb.u,arb.u,arb.u,arb.u,arb.u,arb.u,arb.u,deg,deg,deg,deg,arb.u.,arb.u.\n'
+    # to fit all other files for easy origin importing
+    origin_header = "#\n"*100
+    origin_header += 'Sideband,S0,S0 err,S1,S1 err,S2,S2 err,S3,S3 err,' \
+        + 'alpha,alpha err,gamma,gamma err,DOP,DOP err\n'
+    origin_header += 'Order,arb.u,arb.u,arb.u,arb.u,arb.u,arb.u,arb.u,' \
+        + 'arb.u,deg,deg,deg,deg,arb.u.,arb.u.\n'
     origin_header += 'Sideband,{},{},{},{},{},{},{},{},{},{},{},{},{},{}'.format(*["{}".format(series)] * 14)
     sbFits = sbFits[sbFits[:, 0].argsort()]
 
@@ -660,18 +672,16 @@ def proc_n_fit_qwp_data(data, laserParams = dict(), wantedSBs = None, vertAnaDir
 
         if not os.path.exists(os.path.dirname(save)):
             os.mkdir(os.path.dirname(save))
-        np.savetxt(save, np.array(sbFitsSave), delimiter=',', header=origin_header,
-                   comments='', fmt='%.6e')
-
-    # print("a = {:.2f} ± {:.2f}".format(sbFits[1, 9], sbFits[1, 10]))
-    # print("g = {:.2f} ± {:.2f}".format(sbFits[1, 11], sbFits[1, 12]))
-
+        np.savetxt(
+            save, np.array(sbFitsSave), delimiter=',', header=origin_header,
+            comments='', fmt='%.6e'
+            )
     if plot:
         plt.figure("alpha")
         plt.errorbar(sbFitsDict["alpha"][:, 0],
                      sbFitsDict["alpha"][:, 1],
                      sbFitsDict["alpha"][:, 2],
-                     'o-', name = series
+                     'o-', name=series
                      )
         plt.figure("gamma")
         plt.errorbar(sbFitsDict["gamma"][:, 0],
