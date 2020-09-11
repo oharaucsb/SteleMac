3e9e064c2bc4f341011e38245912ef18c3c2dace
Cameron <C_Cannon@ucsb.edu>
2020-09-11 16:37:16 -0700

    restructuring of setup.py for optional dev dependencies

--- setup.py
+++ setup.py
@@ -4,58 +4,66 @@ from setuptools import find_namespace_packages, setup
 # https://setuptools.readthedocs.io/en/latest/setuptools.html
 
 setup(
-
-    # ***package setup***
-    name='Stele',  # package name
-    version='0.1a2',
-            # what version, pre release codes are 'anything' < 'final'
-            # equivelat post finals, c = pre = preview = rc
-            # post release codes are 'anything' > 'final'
-            # thus 1.0 < 1.1a < 1.1pre < 1.1 < 1.1finally < 1.1g
-    packages=find_namespace_packages(where='src'),
-    # what and where are the packages, conforms to src/ layout
-    # calls setuptools member to automate this, allows setup.py auto update
-    package_dir={'': 'src'},
-    # source directory for the given package
-    include_package_data=True,
-    # should data inside package be included
-    zip_safe=False,
-    # can package be installed from Zip?
-
-    # entry_points={'console_scripts': ['runPEPS = PEPS.command_line:main']},
-    # What is the runPEPS command in use at the above line?
-
-    # ***requirements***
-    python_requires='>=3.5',
-    # required version of python is 3.3 or greater as defined in PEP 440
-    # necessary since this is a namespace package and 2.7 is EOL
-    setup_requires=[],
-    # packages required to run the setup script itself
-    dependency_links=[],
-    # URLS to be searched for setup_requires dependencies
-    install_requires=[
-        'glob2>=0.6',
-        'joblib>=0.13.1',
-        'jsonschema>=2.6.0',
-        'matplotlib>=2.2.2',
-        'numpy>=1.14.3',
-        # style collection used for this distribution, optional dependency
-        'pylama>=7.7.1',
-        'PyQt5==5.14.0',
-        'pyqtgraph>=0.10.0',
-        'scipy>=1.1.0'
-        # automatic documentation support via doclines, optional dependency
-        'Sphinx>=3.2.1'
-        ],
-    # packages required for the distribution package to function
-
     # ***distribution metadata***
     url='https://github.com/C-S-Cannon/Stele',
     author='Cameron Cannon',
     author_email='c_cannon@ucsb.edu',
     license='MIT',
     description="Monolithic python distribution to eventually house ITST code."
-)
 
-# look at utilizing extras for correllated modules that are only required in
-# rare cases but bring many new dependency requirements
+
+    # ***package setup***
+
+    # package name
+    name='Stele',
+    # what version, pre release codes are 'anything' < 'final'
+    # equivelat post finals, c = pre = preview = rc
+    # post release codes are 'anything' > 'final'
+    # thus 1.0 < 1.1a < 1.1pre < 1.1 < 1.1finally < 1.1g
+    version='0.1a2',
+    # what and where are the packages, conforms to src/ layout
+    # calls setuptools member to automate this, allows setup.py auto update
+    packages=find_namespace_packages(where='src'),
+    # source directory for the given package
+    package_dir={'': 'src'},
+    # should data inside package be included
+    include_package_data=True,
+    # can package be installed from Zip?
+    zip_safe=False,
+    # What is the runPEPS command in use at this line?
+    # entry_points={'console_scripts': ['runPEPS = PEPS.command_line:main']},
+
+
+    # ***requirements***
+
+    # required version of python is 3.3 or greater as defined in PEP 440
+    # necessary since this is a namespace package and 2.7 is EOL
+    python_requires='>=3.5',
+    # packages required to run the setup script itself
+    setup_requires=[],
+    # URLS to be searched for setup_requires dependencies
+    dependency_links=[],
+    # packages required for the distribution package to function
+    install_requires=[
+        'glob2>=0.6',
+        'joblib>=0.13.1',
+        'jsonschema>=2.6.0',
+        'matplotlib>=2.2.2',
+        'numpy>=1.14.3',
+        'PyQt5==5.14.0',
+        'pyqtgraph>=0.10.0',
+        'scipy>=1.1.0'
+        ],
+    # dependencies that are necessary for additional usages of the distro
+    extras_requires={
+        # dependencies for development of the distribution package
+        "dev": [
+            # style collection used for this distribution, optional dependency
+            'pylama>=7.7.1',
+            # automatic documentation support via doclines, optional dependency
+            'Sphinx>=3.2.1'
+            ]
+        # TODO: Look at utilizing extras for correllated modules that are only
+        #   required in rare cases but bring many new dependency requirements.
+        }
+)
