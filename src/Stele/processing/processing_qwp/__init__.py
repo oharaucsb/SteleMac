5081051ca6229bbdbc3f29e6a23b0da5043f798f
Cameron <C_Cannon@ucsb.edu>
2020-11-05 14:32:14 -0800

    corrected improper function calls of processing_qwp

--- src/Stele/processing/processing_qwp/__init__.py
+++ src/Stele/processing/processing_qwp/__init__.py
@@ -6,7 +6,7 @@ __author__ = 'Sphinx'
 expFanCompiler broken into fanCompiler, fanCompilerWOStokes, jonesToFans,
     and SbStateGetter
 """
-
+"""
 from . import createFanDiagram
 from . import expFanCompiler
 from . import extractMatrices
@@ -14,3 +14,4 @@ from .fanDiagram import FanDiagram
 from . import interactiveJMatrixExtraction
 from . import monteCarloTMatrices
 from . import polarPlot
+"""
