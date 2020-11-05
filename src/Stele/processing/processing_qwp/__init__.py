82111833fc2cc799439025294a0e2f0d54ce5f01
Cameron <C_Cannon@ucsb.edu>
2020-11-05 14:36:10 -0800

    import fan_compiler into processing_qwp

--- src/Stele/processing/processing_qwp/__init__.py
+++ src/Stele/processing/processing_qwp/__init__.py
@@ -6,12 +6,12 @@ __author__ = 'Sphinx'
 expFanCompiler broken into fanCompiler, fanCompilerWOStokes, jonesToFans,
     and SbStateGetter
 """
-"""
-from . import createFanDiagram
-from . import expFanCompiler
-from . import extractMatrices
-from .fanDiagram import FanDiagram
-from . import interactiveJMatrixExtraction
-from . import monteCarloTMatrices
-from . import polarPlot
-"""
+from . import fan_compiler
+
+# from . import createFanDiagram
+# from . import expFanCompiler
+# from . import extractMatrices
+# from .fanDiagram import FanDiagram
+# from . import interactiveJMatrixExtraction
+# from . import monteCarloTMatrices
+# from . import polarPlot
